a5ab7002f683576db0d1b935ea6751813d84db56
Haseeb <haseebusman1301@gmail.com>
2023-08-15 12:05:04 +0500

    CompleteRegLog

--- sms/views.py
+++ sms/views.py
@@ -10,23 +10,24 @@ def signup (request):
     if request.method == 'POST':
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
-        email = request.POST['email']       
-        password1 = request.POST['password1']
-        password2 = request.POST['password2']
+        email = request.POST['email']
+        username = request.POST['username']       
+        password1 = request.POST['psw']
+        # password2 = request.POST['password2']
 
-        data = User.objects.create_user(first_name = first_name, last_name = last_name, email = email, password = password1)
+        data = User.objects.create_user(first_name = first_name, last_name = last_name, email = email, password = password1, username = username)
         data.save()
         return redirect('login')
     return render(request, 'signup.html')
 
 def login(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = auth.authenticate (username = username, password = password)
+        username = request.POST['uname']
+        password = request.POST['psw']
+        user = auth.authenticate(username = username, password = password)
 
         if user is not None:
-            auth.login(request.user)
-            return render('home')        
+            auth.login(request, user)
+            return redirect('home')        
 
     return render(request, 'login.html')
